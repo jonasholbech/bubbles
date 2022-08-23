a2881f0990cb4a63c7776664ed9146c146f78e29
Jonas Holbech <jh@jonasholbech.dk>
2022-08-23 11:42:35 +0200

    set rabndom color

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ document.querySelectorAll("div").forEach((div) => {
   div.style.transform = `translate(${Math.random() * 1000}px, ${
     Math.random() * 1000
   }px)`;
+  div.style.backgroundColor = `rgb(${Math.random() * 255}, ${
+    Math.random() * 255
+  }, ${Math.random() * 255})`;
 });
