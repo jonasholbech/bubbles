d827cb7c45c8cdc6da4a9de89d525b9fcd779101
Jonas Holbech <jh@jonasholbech.dk>
2022-08-23 13:21:44 +0200

    add mouseenter

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+document.querySelectorAll("div").forEach((div) => {
+  div.addEventListener("mouseenter", () => {
+    div.style.transform = `translate(${Math.random() * 100}vw, ${
+      Math.random() * 100
+    }vh)`;
+    div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
+  });
+});
 setInterval(() => {
   document.querySelectorAll("div").forEach((div) => {
     div.style.transform = `translate(${Math.random() * 100}vw, ${
